6c44716ff5d4bff4603e08c00b0ca29b702ccd92
Elias <e.duvernois2016@gmail.com>
2025-01-30 21:59:56 +0100

    Ajout des entités sans les relations

---
+++ src/Entity/WatchHistory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WatchHistoryRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WatchHistoryRepository::class)]
+class WatchHistory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $last_watched = null;
+
+    #[ORM\Column]
+    private ?int $number_of_view = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastWatched(): ?\DateTimeInterface
+    {
+        return $this->last_watched;
+    }
+
+    public function setLastWatched(\DateTimeInterface $last_watched): static
+    {
+        $this->last_watched = $last_watched;
+
+        return $this;
+    }
+
+    public function getNumberOfView(): ?int
+    {
+        return $this->number_of_view;
+    }
+
+    public function setNumberOfView(int $number_of_view): static
+    {
+        $this->number_of_view = $number_of_view;
+
+        return $this;
+    }
+}
